125bffc104d7eb0070e67ca35fbe55b2b61e3b62
Anon <24657656+Anon20198@users.noreply.github.com>
2018-03-09 11:57:46 +0100

    Add files via upload

---
+++ src/EasterEgg.java
@@ -0,0 +1,120 @@
+public class EasterEgg {
+
+  public void TrollFace() {
+    trollFace();
+  }
+
+  public void XmassTree() {
+      xmassTree();
+  }
+
+  private void trollFace() {
+    System.out.println();
+    System.out.println("....................................................................................................");
+    System.out.println(".................................mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm.......................");
+    System.out.println(".........................mmmmmmmmmmmmmmmm..................................mmmmm....................");
+    System.out.println("...................mmmmmmmmmmm..........mmmmm...............mm................mmm...................");
+    System.out.println(".................mmmm............................................mm.............mmm.................");
+    System.out.println("..............mmmm....m..mmmmmm............mmm..............mm......m............mmm................");
+    System.out.println("............mmmm.................................................mm....m..........mmm...............");
+    System.out.println("...........mmm............mmmmm....................m..........mmm...m....m.........mmm..............");
+    System.out.println("..........mm...........m.........................m................mm...m....m.......mmm.............");
+    System.out.println("..........mm.........m............m..................................m..m....m.......mm.............");
+    System.out.println("..........mm........m..................................................m..m...........mm............");
+    System.out.println(".........mm..........................................mmmmmmmmmmmmmm...................mmm...........");
+    System.out.println("........mmm............mmmmmmmm...................mmmmm...mmmmmmmmmmmm.................mmm..........");
+    System.out.println("....mmmm...........mmmmmmmmmmmmmm.............mmmm......mmmmmmmm..mmm..................mmm..........");
+    System.out.println("...mmm...mmmmm.mmm.mmmmmmmmmmmmmmm...........mmm.....mmmmmmmmmmmmmmmmm....m....mmmmmmmm.mmmmm.......");
+    System.out.println("..mm...m..................mmmmmmmmmmmmm.......mmmmmmmmm...........mm...m..................mmmm......");
+    System.out.println(".mm..m...mm.....................mmmm...........mmmmm......mmm..............mmmmmmmmmm........mm.....");
+    System.out.println(".mm.m..m...mmmmmm................mm.........................mmmm.......mmmmmm......mmmm.....m.mm....");
+    System.out.println(".mmm.....mmmmmmmmmm....m.........mm...........................mmmmmmmmmmm.....mm.....mmm....m..mmm..");
+    System.out.println(".mmm.....m.......mmmmmmmm........mm...........................................mm......mm....m..mmm..");
+    System.out.println(".mmm..m.......mm..mmmm........mmmm.........................................mmmmm.......mm...m...mm..");
+    System.out.println(".mm....m......mm...........mmmm................mmmmmmmm................mmmmm...mmmmmm..mm...m...mm..");
+    System.out.println(".mmmm...mm..mmmm..........mmmmm....................mm..............mmmmmm.....mmm.mmm.mmm...m..mmm..");
+    System.out.println("..mm.mm.....mmmm.......mm..mmmmm.........mmmmmmm...mm..........mmmmmmm........mm......mm.......mmm..");
+    System.out.println("..mmm....m..mm.mmm...m........mmm..............m.mmm......mmmmmmmm.mm.......mmmm.....mm...mm..mmm...");
+    System.out.println("...mmm.....mmmmmmmmm............mmmmmm...............mmmmmmmm......mm....mmmmmm.........mmm..mm.....");
+    System.out.println("....mmm....mm.mm.mmmmmm...........mmm...........mmmmmmmm..........mmm.mmmmmmmm.......m......mmm.....");
+    System.out.println(".....mm....mmmm..mm..mmmmmmmm........mmmmmmmmmmmmmm..mm..........mmmmmmm..mmm..........mmmmmm.......");
+    System.out.println(".....mm....mmmm..mm....mmmmmmmmmmmmmmmmmmmm..........mm......mmmmmmmm....mmm............mmmm........");
+    System.out.println(".....mm....mmmm..mm...mmm.......mm.......mm..........mmm.mmmmmmmmm.mm...mmm...........mmm...........");
+    System.out.println(".....mm....mmmmmmmm...mm........mm.......mm.........mmmmmmmmmmm....mm..mmm...........mmm............");
+    System.out.println(".....mm....mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm.......mmmmmm............mm.............");
+    System.out.println(".....mm....mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm.mm...........mmm.............mmm.............");
+    System.out.println(".....mm.....mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm.......mm.........mmm..............mmm..............");
+    System.out.println(".....mm.....mm.mmmmmmmmmmmmmmmmmmmmmmmmmmm.mm..........mmm.....mmmm...............mmm...............");
+    System.out.println(".....mm......mm.mm..mm...mm.....mmm........mm...........mm...mmmm...............mmm.................");
+    System.out.println(".....mm......mmmmmm.mmm...mm.....mm........mm............mmmmmm................mmm..................");
+    System.out.println(".....mm.......mmmm...mmm..mmm....mm........mm.........mmmmmm.......m.....m...mmm....................");
+    System.out.println(".....mm.........mmmmmmmmm..mm....mmm.......mm..mmmmmmmmm........m.....mm...mmmm.....................");
+    System.out.println(".....mm..............mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm.........mm....mm....mmmm.......................");
+    System.out.println("....mmm........m..........................................m.....mm....mmmmm.........................");
+    System.out.println("....mm..........m....................................mm......m.....mmmmm............................");
+    System.out.println("....mm............m..............................mm......mm.....mmmmm...............................");
+    System.out.println("..mm.....mm........mmm...........mmmmmmmmmm......mm.......mmmmmm....................................");
+    System.out.println(".mm.........m.........................mmm............mmmmm..........................................");
+    System.out.println(".mmm..........mmmmmmmmmmmmmmm.....................mmmmm.............................................");
+    System.out.println(".mmm..........................................mmmmm.................................................");
+    System.out.println("..mmm...................................mm.mmmmm....................................................");
+    System.out.println("...mmmm............................mmmmmmmmmm.......................................................");
+    System.out.println("....mmmmm...................mmmmmmmmm...............................................................");
+    System.out.println(".......mmmmmmmmmmmmmmmmmmmmmmmm.....................................................................");
+    System.out.println("....................................................................................................");
+    System.out.println();
+  }
+
+  private void xmassTree() {
+    System.out.println();
+    System.out.println("                            $                                                       ");
+    System.out.println("                           :$$                                                      ");
+    System.out.println("                      seeee$$$Neeee                                                 ");
+    System.out.println("                        R$$$F$$$$F                                                  ");
+    System.out.println("                          $$$$$$                                                    ");
+    System.out.println("                         @$$P*$$B                                                   ");
+    System.out.println("                        z$#''  $#$b                                                 ");
+    System.out.println("                        '' d   'N ''                                                ");
+    System.out.println("                         @''     ?r                                                 ");
+    System.out.println("                       xF .       N                                                 ");
+    System.out.println("                    .$> P54.R       `$                                              ");
+    System.out.println("                  $*   '*''$$$  uoP***~                                             ");
+    System.out.println("                   #Noo ''?$N'   #oL                                                ");
+    System.out.println("                      f       o$#$$''e.                                             ");
+    System.out.println("                     $  @b    hoR$$r ^''$$b                                         ");
+    System.out.println("                  .M   ?B$E   *.B$$       .R                                        ");
+    System.out.println("                .*     * *.4*R         ..*                                          ");
+    System.out.println("             oo#     ooL    d#R.     P##~                                           ");
+    System.out.println("             $c    .''P#$  @   P     k                                              ");
+    System.out.println("               R$r ''?L$$  P  ''r     'N                                            ");
+    System.out.println("                 ^$ ''$$$` $.....JL     ''N.                                        ");
+    System.out.println("               .$|           * P5''LR      $..                                      ");
+    System.out.println("            ..* 4*R     xr    'PFN$$   .k    ''*****.                               ");
+    System.out.println("         od#''   d#*.  ''*$$P~   ''?$*' '#$$$'      u'                              ");
+    System.out.println("      e'''      f   M   @F''$  ec       x$''$.     :                                ");
+    System.out.println("      M        >  ''d       $$$$?$           .$$F`                                  ");
+    System.out.println("       P..  .''.....'L $$.'''. ''   ''$$   $''.                                     ");
+    System.out.println("          '**..  *   R..$$ `R$*k.  fdM$$>     *..                                   ");
+    System.out.println("            J''       *k$$$~  ''*$**o$o$$P        '*oo.                             ");
+    System.out.println("           P           #        ''$$$#*o          >  '####*oooo                     ");
+    System.out.println("        .e  ''          :e$$e.  F3  ^''$P  :$$s :e@$ee         s'                   ");
+    System.out.println("      $P` M7>    $P$$k ''$''?$3 @''#N      CxN$$> .$$$       .P                     ");
+    System.out.println("   M$~   J##   44N>$$  .d$.$d   @&      `$$$  F  .8..$$$*                           ");
+    System.out.println("   .***     :   JM   *d$$*.$$.P  M  .P5     M          **.                          ");
+    System.out.println("   ''oo      J  .dP    ud$$od#   $oooooo$  oo$oo           ###ou                    ");
+    System.out.println("      ''####$beeeee$.'$eeP#~        ''''      $$$.    e$$$o       #heeee            ");
+    System.out.println("         :''    '' z$r ^            o$N     ''  ''   4$z>$$             ''''#$$$    ");
+    System.out.println("        .~      F$4$B       r    F @#$.       ..   $8$$P M7                $        ");
+    System.out.println("      .*  $     8 $$B     .J$..  hP$$$F     .'PB$       J~##             .d~        ");
+    System.out.println("    .P  *$$$*    ''*''       $$$    #**~      hdM$$>     <   JM.......*****         ");
+    System.out.println("  .P     $#*k       .o#>  P'' 'k   ..         '$$P      d  .JP'h                    ");
+    System.out.println(" ''''hr ^        xe'''  >          '''c           ee    @beeeee$.)                  ");
+    System.out.println("       ''''t$$$$F''      M        $`   R          > ''$r     ''     ''c             ");
+    System.out.println("                           oooooooooo                                               ");
+    System.out.println("                           z        z                                               ");
+    System.out.println("                           z.,ze.$$$z                                               ");
+    System.out.println();
+
+  }
+
+}
